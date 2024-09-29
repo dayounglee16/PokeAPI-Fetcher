73ffd9837ca074c72c8e2839df6a16cd03eadf59
dayounglee <id46827@gamil.com>
2024-09-29 23:39:37 +0900

    main.tsx로 확장자명 변경 후 오류 해결

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// export default로 불러오지 않는 라이브러리 오류 => "esModuleInterop": true, 옵션추가
+// 경로 jsx 오류 => "jsx": "react-jsx", "allowJs": true 옵션 추가
